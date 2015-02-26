f1f5040e1e99b8144855ce9e160ad49687962a36
Luke Martin <Luke.Martin@akqa.com>
2015-02-26 12:07:31 +0000

    Add some ES6 goodness

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-let k = 'cake';
+import {Person} from './module'
+
+let luke = new Person('Luke');
+luke.think(1000).then(() => {
+        luke.greet();
+    })
+    .catch(err => {
+        console.error(err);
+    });
--- src/module.js
+++ src/module.js
@@ -1,1 +1,13 @@
-let c = 'choose';
+export class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    greet() {
+        console.log(`Hi, ${this.name}`);
+    }
+    think(duration) {
+        return new Promise((resolve, reject) => {
+            setTimeout(resolve, duration);
+        });
+    }
+}
